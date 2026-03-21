c95c41e25ebdaca52638f679fa5c8e46b8835aff
Jorge Fernandes <mrmowinckel@gmail.com>
2026-03-21 15:18:14 +0000

    muda retorno de /status para json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
 function status(request, response) {
   response.setHeader("Content-Type", "text/plain; charset=utf-8");
-  response.status(200).send("é realmente uma benção isso aqui!");
+  response.status(200).json({ message: "é realmente uma benção isso aqui!" });
 }
 
 export default status;
