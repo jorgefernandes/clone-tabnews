3ac0ade776868a62082ecf7c8a961ff5aa270e04
Jorge Fernandes <mrmowinckel@gmail.com>
2026-03-23 13:26:07 +0000

    Cria conexa com db no client e seta .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1");
+
   response.setHeader("Content-Type", "text/plain; charset=utf-8");
   response.status(200).json({ message: "é realmente uma benção isso aqui!" });
 }
