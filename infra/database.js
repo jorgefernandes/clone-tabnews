4f0bc61c269374e6041ec79b47b740b04c92ef4a
Jorge Fernandes <mrmowinckel@gmail.com>
2026-03-30 10:44:24 +0000

    remove console.log do `database.js`

--- infra/database.js
+++ infra/database.js
@@ -12,15 +12,6 @@ async function query(queryObject) {
     ssl: process.env.ENVIRONMENT === "development" ? false : true,
   });
 
-  console.log("Dados de conexão ao banco", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.ENVIRONMENT === "development" ? false : true,
-  });
-
   try {
     await client.connect();
 
