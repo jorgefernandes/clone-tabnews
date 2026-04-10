dafd5fd932fca75baac1935ffc2b3733f4859885
Jorge Fernandes <mrmowinckel@gmail.com>
2026-04-10 11:03:24 +0000

    Adiciona try/catch na controller migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -5,41 +5,47 @@ import database from "infra/database.js";
 export default async function migrations(request, response) {
   const dbConnection = await database.databaseConnection();
 
-  const migrationRunnerConfig = {
-    dbClient: dbConnection,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    dryRun: false,
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  try {
+    const migrationRunnerConfig = {
+      dbClient: dbConnection,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      dryRun: false,
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationsRunner({
-      ...migrationRunnerConfig,
-    });
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationsRunner({
+        ...migrationRunnerConfig,
+      });
 
-    dbConnection.end();
+      dbConnection.end();
 
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+
+      return response.status(200).json(migratedMigrations);
     }
 
-    return response.status(200).json(migratedMigrations);
-  }
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationsRunner({
+        ...migrationRunnerConfig,
+        dryRun: true,
+      });
 
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationsRunner({
-      ...migrationRunnerConfig,
-      dryRun: true,
-    });
+      dbConnection.end();
+
+      return response.status(200).json(pendingMigrations);
+    }
 
     dbConnection.end();
 
-    return response.status(200).json(pendingMigrations);
+    return response.status(405).end();
+  } catch (error) {
+    //
+  } finally {
+    dbConnection.end();
   }
-
-  dbConnection.end();
-
-  return response.status(405).end();
 }
